480135a2ce7b3eff2cd7e4505fbdb4b5f8615cd4
LucaLF-dev <laficaraluca@gmail.com>
2023-10-23 19:00:44 +0200

    Add - input and photo

--- js/app.js
+++ js/app.js
@@ -3,7 +3,9 @@ createApp({
 	data() {
 		return {
 			message: "Hello World",
+            messageWorld: "Cosa pensi del mondo in cui viviamo?",
 			num: 26,
+            photo: "https://picsum.photos/200/300",
 		};
 	},
     methods: {
